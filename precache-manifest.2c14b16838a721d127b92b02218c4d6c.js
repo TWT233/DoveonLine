e6a8c236721d842589f12cf503e440e8edd57b7f
TWT <twt2333@outlook.com>
2020-10-08 16:34:19 +0800

    [deploy]2020-10-08 16:34:18

---
+++ precache-manifest.2c14b16838a721d127b92b02218c4d6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d04233278c40698b67",
+    "url": "/DoveonLine/css/chunk-vendors.7ca4ce14.css"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/DoveonLine/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/DoveonLine/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/DoveonLine/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/DoveonLine/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "683b37084a5ac362c126d28b09f3e38b",
+    "url": "/DoveonLine/index.html"
+  },
+  {
+    "revision": "021e567ea90b1aa7a699",
+    "url": "/DoveonLine/js/app.ba957875.js"
+  },
+  {
+    "revision": "85d04233278c40698b67",
+    "url": "/DoveonLine/js/chunk-vendors.858512fe.js"
+  },
+  {
+    "revision": "a348e23ea6b2c0d4f477e874ea1ae47c",
+    "url": "/DoveonLine/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DoveonLine/robots.txt"
+  }
+]);
